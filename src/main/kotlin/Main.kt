08838a75808709e88bae70ff6b9162f27ec5bea9
fernandorosa <stevensfrr@gmail.com>
2023-02-05 23:47:02 -0500

    Extension functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,30 @@
-class Quiz {
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+
+}
+class Quiz: ProgressPrintable{
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+
     companion object StudentProgress {
         var total: Int = 10
-        var answered: Int = 10
+        var answered: Int = 3
+    }
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println()
+        println(progressText)
     }
 }
-val Quiz.StudentProgress.progressText: String
-    get() = "${answered} of ${total} answered"
+//val Quiz.StudentProgress.progressText: String
+//    get() = "${answered} of ${total} answered"
 
 data class Question<T>(
     val questionText: String,
@@ -19,14 +34,14 @@ data class Question<T>(
 enum class Difficulty {
     EASY, MEDIUM, HARD
 }
-fun Quiz.StudentProgress.printProgressBar() {
-    repeat(Quiz.answered) { print("▓") }
-    repeat(Quiz.total - Quiz.answered) { print("▒") }
-    println()
-    println(Quiz.progressText)
-}
+//fun Quiz.StudentProgress.printProgressBar() {
+//    repeat(Quiz.answered) { print("▓") }
+//    repeat(Quiz.total - Quiz.answered) { print("▒") }
+//    println()
+//    println(Quiz.progressText)
+//}
 fun main() {
     //println(Quiz.progressText)
-    Quiz.printProgressBar()
-
+    //Quiz.printProgressBar()
+    Quiz().printProgressBar()
 }
