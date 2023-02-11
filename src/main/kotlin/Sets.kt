312d00c7ea59ed264c995a3eae4e8b7642014eec
fernandorosa <stevensfrr@gmail.com>
2023-02-11 18:03:11 -0500

    Data Sets

---
+++ src/main/kotlin/Sets.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
+    solarSystem.add("Pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
+    solarSystem.add("Pluto")
+    println(solarSystem.size)
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
+
+
+}
